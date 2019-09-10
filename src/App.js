07ea34648beda89ad4a3305644e07b9204b1fd39
abhisofriendly <abhisofriendly@gmail.com>
2019-09-10 08:12:29 +0530

    pop up react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
+import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dialog from './components/Dialog';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    isOpen:false
+  }
+  render(){
+    return (
+      <div className="App">
+        <button onClick={(e)=>this.setState({isOpen:true})}>Open Dialog</button>
+        <Dialog isOpen={this.state.isOpen} onClose={(e)=>this.setState({isOpen:false})}>
+          Lorem ipsum sit amet conseecteur
+          Lorem ipsum sit amet conseecteur
+        </Dialog>
+      </div>
+    );
+  } 
 }
 
 export default App;
